1791ff531fe630b4977ea3e980d38cc092a4fe2c
J-Crawford89 <J.Crawford0120@Gmail.com>
2020-05-04 19:14:29 -0400

    IPlanetService

--- Contracts/IPlanetService.cs
+++ Contracts/IPlanetService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models.PlanetModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Contracts
 {
     public interface IPlanetService
     {
+        void CreatePlanet(PlanetCreateModel planetToCreate);
+        IEnumerable<PlanetListItem> GetPlanets();
+        PlanetDetailModel GetPlanetDetailById();
+        void UpdatePlanet(PlanetUpdateModel planetToUpdate);
+        void DeletePlanet(PlanetDeleteModel planetToDelete);
     }
 }
