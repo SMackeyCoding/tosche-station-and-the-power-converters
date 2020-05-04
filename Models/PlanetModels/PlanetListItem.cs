1211c3b4e14cc772d66907086564f526e5aad473
J-Crawford89 <J.Crawford0120@Gmail.com>
2020-05-04 19:13:20 -0400

    planet models

---
+++ Models/PlanetModels/PlanetListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.PlanetModels
+{
+    public class PlanetListItem
+    {
+        public int PlanetId { get; set; }
+        public string PlanetName { get; set; }
+        public string PlanetDescription { get; set; }
+    }
+}
---
+++ Models/PlanetModels/PlanetUpdateModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.PlanetModels
+{
+    public class PlanetUpdateModel
+    {
+        public int PlanetId { get; set; }
+        public string UpdatedPlanetName { get; set; }
+        public string UpdatedPlanetDescription { get; set; }
+        public string UpdatedPlanetClimate { get; set; }
+        public string UpdatedHoursPerDay { get; set; }
+        public string UpdatedDaysPerYear { get; set; }
+        public string UpdatedNumberOfSuns { get; set; }
+        public string UpdatedNumberOfMoons { get; set; }
+    }
+}
