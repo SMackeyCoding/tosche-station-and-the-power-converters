c9f1980ced13a69af760edc13342501172a3515f
SMackeyCoding <s.Mack.coding@gmail.com>
2020-05-04 20:25:21 -0400

    Updated Character Properties

--- Data/Entities/Character.cs
+++ Data/Entities/Character.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,17 @@ namespace Data.Entities
 {
     class Character
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Affiliation { get; set; }
+        public string WeaponOfChoice { get; set; }
+        [ForeignKey ("Planet")]
+        public int PlanetId { get; set; }
+        [ForeignKey ("Ship")]
+        public int ShipId { get; set; }
     }
 }
