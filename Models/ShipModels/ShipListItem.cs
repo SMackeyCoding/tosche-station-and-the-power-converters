6958644a427c0649ab8ea7a7fba076adac307af4
Loretta Tappan <lorettatappan@gmail.com>
2020-05-04 20:22:00 -0400

    Added ShipUpdateModel

---
+++ Models/ShipModels/ShipListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ShipModels
+{
+    public class ShipListItem
+    {
+        public int ShipId { get; set; }
+        public string ShipName { get; set; }
+        public string ShipModel { get; set; }
+    }
+}
---
+++ Models/ShipModels/ShipUpdateModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ShipModels
+{
+    public class ShipUpdateModel
+    {
+        public int ShipID { get; set; }
+        public string ShipName { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Length { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+}
